3db99f8e5e689630033b3c9f37ea7c5d94eeb425
amigin <amigin@gmail.com>
2022-06-21 11:38:21 +0300

    Swagger routes went in front of

--- src/http/start_up.rs
+++ src/http/start_up.rs
@@ -11,9 +11,9 @@ pub fn setup_server(app: &Arc<AppContext>) {
 
     let swagger_middleware = crate::http::SwaggerMiddleware::new(app.clone());
 
-    http_server.add_middleware(Arc::new(swagger_middleware));
-
     http_server.add_middleware(Arc::new(SwaggerRouterMiddleware::new(app.clone())));
+
+    http_server.add_middleware(Arc::new(swagger_middleware));
 
     http_server.start(app.clone());
 }
