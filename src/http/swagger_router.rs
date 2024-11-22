418271c874d50cba741b382f0c7c1d71bf33480e
amigin <amigin@gmail.com>
2024-11-22 16:40:37 +0200

    my-http-server:0.8.2

--- src/http/swagger_router.rs
+++ src/http/swagger_router.rs
@@ -27,11 +27,10 @@ impl HttpServerMiddleware for SwaggerRouterMiddleware {
 
         for route in self.app.settings.get_routes().await {
             if route.url == path.as_str() {
-                let mut fl_url = flurl::FlUrl::new(route.remote_url.as_str());
+                let mut fl_url = flurl::FlUrl::new(route.remote_url.as_str()).with_retries(3);
 
                 if let Some(host) = ctx.request.get_headers().try_get_case_insensitive("host") {
                     let host = host.as_str().unwrap();
-                    println!("Overridden host: {}", host);
                     fl_url = fl_url.with_header("host", host);
                 }
 
